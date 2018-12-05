0927929221461337ceecd03a6e13fe973373a9f8
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-06 00:00:56 +0100

    code cleanup

--- train.py
+++ train.py
@@ -296,8 +296,7 @@ def main():
         epoch_batch_iter_count = 0
 
         if lr_scheduler_type == "lr_finder":
-            r = current_sgdr_cycle_epochs / min(current_sgdr_cycle_epochs, (sgdr_iterations + 1) / epoch_iterations)
-            new_lr = lr_min + r * (lr_max - lr_min)
+            new_lr = lr_min + min(1, sgdr_iterations / epoch_iterations) * (lr_max - lr_min)
             adjust_learning_rate(optimizer, new_lr)
 
         for b, batch in enumerate(train_set_data_loader):
