d35121f3033ed8e417688f4acdc2480050e2d635
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-05 18:54:40 +0100

    added a sigmoid based focal loss implementation

--- metrics/focal_loss.py
+++ metrics/focal_loss.py
@@ -4,20 +4,38 @@ import torch.nn.functional as F
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=1.2):
+    def __init__(self, gamma=2, size_average=True):
         super().__init__()
         self.gamma = gamma
-        self.alpha = alpha
+        self.size_average = size_average
 
-    def forward(self, logits, labels):
-        eps = 1e-7
+    def forward(self, logit, target):
+        target = target.view(-1, 1).long()
 
-        # loss =  - np.power(1 - p, gamma) * np.log(p))
-        probs = F.softmax(logits)
-        probs = probs.gather(dim=1, index=labels.view(-1, 1)).view(-1)
-        probs = torch.clamp(probs, min=eps, max=1 - eps)
+        if self.weight is None:
+            self.weight = [1] * 28
 
-        loss = -torch.pow(1 - probs, self.gamma) * torch.log(probs)
-        loss = loss.mean() * self.alpha
+        prob = F.sigmoid(logit)
+        prob = prob.view(-1, 1)
+        prob = torch.cat((1 - prob, prob), 1)
+        select = torch.FloatTensor(len(prob), 2).zero_().cuda()
+        select.scatter_(1, target, 1.)
+
+        self.weight = torch.FloatTensor(self.weight).cuda().view(-1, 1)
+        self.weight = torch.gather(self.weight, 0, target)
+
+        prob = (prob * select).sum(1).view(-1, 1)
+        prob = torch.clamp(prob, 1e-8, 1 - 1e-8)
+
+        focus = torch.pow((1 - prob), self.gamma)
+        # focus = torch.where(focus < 2.0, focus, torch.zeros(prob.size()).cuda())
+        focus = torch.clamp(focus, 0, 2)
+
+        batch_loss = - self.weight * focus * prob.log()
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss
 
         return loss
