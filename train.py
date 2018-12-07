77f5eda6ae7f1a49466348bd764c446ec8cb7e42
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-07 23:02:15 +0100

    use focal loss by default

--- train.py
+++ train.py
@@ -493,7 +493,7 @@ if __name__ == "__main__":
     argparser.add_argument("--model", default="resnet34")
     argparser.add_argument("--patience", default=5, type=int)
     argparser.add_argument("--optimizer", default="sgd")
-    argparser.add_argument("--loss", default="f1")
+    argparser.add_argument("--loss", default="focal")
     argparser.add_argument("--use_class_weights", default=False, type=str2bool)
     argparser.add_argument("--sgdr_cycle_epochs", default=5, type=int)
     argparser.add_argument("--sgdr_cycle_epochs_mult", default=1.0, type=float)
